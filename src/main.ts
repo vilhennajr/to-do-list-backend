79248397eeec1fdb5db5198201067d253cde73e2
Flaviano Vilhena <flavianovilhenna@gmail.com>
2024-10-14 20:51:53 -0300

    feat(task-1): creation of the initial structure

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+    credentials: true,
+  });
+
+  await app.listen(3005);
+}
+
+bootstrap();
---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,9 @@
+// prisma.module.ts
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService], // Exportar o PrismaService
+})
+export class PrismaModule {}
